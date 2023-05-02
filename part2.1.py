acc24ec08b6b1f837fc37c408d30a5c7672e907e
xuezun114514 <120536558+xuezun114514@users.noreply.github.com>
2023-05-02 18:52:46 +0100

    part2
    This is the part2.1 that I completed, and it needs to be compatible with the rest of the part2 that you uploaded later, and there is still an error at the end, which is being processed.

---
+++ part2.1.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  2 18:45:45 2023
+
+@author: MSI
+"""
+
+import argparse
+
+def solve_sudoku(grid, explain=False):
+    """
+    Solve Sudoku problem using backtracking.
+    :param grid: a list of m x m, representing the Sudoku puzzle, where 0 indicates an empty cell.
+    :param explain: whether to show each step of the solving process. Default is False.
+    :return: the solved puzzle if there exists a solution; otherwise, return None.
+    """
+    n = len(grid)
+    m = int(n ** 0.5)  # determine the size of each sub-grid
+
+    def find_empty():
+        """
+        Find the position of the first empty cell in the Sudoku puzzle.
+        :return: the position (row, col) of the empty cell if found; otherwise, return None.
+        """
+        for row in range(n):
+            for col in range(n):
+                if grid[row][col] == 0:
+                    return row, col
+        return None
+
+    def is_valid(num, row, col):
+        """
+        Check whether it is valid to place num in the (row, col) cell of the Sudoku puzzle.
+        :param num: the number to be placed.
+        :param row: the row number of the cell.
+        :param col: the column number of the cell.
+        :return: True if it is valid to place num in the cell; otherwise, return False.
+        """
+        # check row and column
+        for i in range(n):
+            if grid[row][i] == num or grid[i][col] == num:
+                return False
+        # check sub-grid
+        box_row = (row // m) * m
+        box_col = (col // m) * m
+        for i in range(m):
+            for j in range(m):
+                if grid[box_row + i][box_col + j] == num:
+                    return False
+        return True
+
+    def solve():
+        """
+        Solve the Sudoku puzzle using backtracking.
+        :return: True if a solution is found; otherwise, return False.
+        """
+        pos = find_empty()
+        if not pos:
+            return True
+        row, col = pos
+        for num in range(1, n + 1):
+            if is_valid(num, row, col):
+                grid[row][col] = num
+                if explain:
+                    print(f"Place {num} at ({row}, {col});")
+                if solve():
+                    return True
+                grid[row][col] = 0
+        return False
+
+    if solve():
+        return grid
+    else:
+        return None
+
+def print_grid(grid):
+    """
+    Print the current state of the Sudoku puzzle.
+    """
+    for row in grid:
+        print(row)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Solve Sudoku")
+    parser.add_argument("--explain", action="store_true", help="Show each step of the solving process")
+    args = parser.parse_args()
+
+    # solve Sudoku
+    grid = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+
+    solution = solve_sudoku(grid, explain=args.explain)
+
+    # output the solution
+    if solution:
+       print_grid(solution)
+    else:
+       print("No solution found.")
